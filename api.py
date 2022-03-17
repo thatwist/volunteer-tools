3b57c5e8cb3795323ded527b9f86a8526c92f8d6
Yuri Ostapchuk <twist522@gmail.com>
2022-03-17 10:55:01 +0200

    bump

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, json, send_from_directory, request
+from flask import Flask, send_from_directory, request
+import json
 from flask.helpers import safe_join
 from telethon.sync import TelegramClient
 from dotenv import load_dotenv
@@ -26,14 +27,12 @@ chats=[
     "UkrzalInfo"
 ]
 
-@api.route('/search', methods=['GET'])
-async def get_results():
-  query = request.args.get("query")
+async def teleQuery(query = None, limit = None):
   async with TelegramClient(tg_name, api_id, api_hash) as client:
     data = []
     for chat in chats:
       print(f"searching {chat}...")
-      async for message in client.iter_messages(entity = chat, search = query):
+      async for message in client.iter_messages(entity = chat, search = query, limit = limit):
         #print(dir(message))
         #print(message.sender_id, ':', message.text)
         # todo - this could be a configuration
@@ -42,18 +41,41 @@ async def get_results():
         # 'entities','views','forwards','replies','edit_date','post_author','grouped_id','restriction_reason','ttl_period'
         # todo - view layer?
         msg = message.to_dict()
+        if not msg.get("message"):
+          continue
         msg_dict = {x: msg[x] for x in ["id", "message", "views", "date"]}
+        if msg_dict["views"] is None:
+          msg_dict[""]
         msg_dict["channel"]=chat
+        msg_dict["ts"]=msg_dict["date"].timestamp()
         msg_dict["platform"]="tg-channel"
         msg_dict["category"]="other"
+        msg_dict["post_id"] = msg_dict["id"]
         msg_dict["title"] = msg_dict["message"][0:50]
         msg_dict["description"] = msg_dict["message"][0:200]
         msg_dict["full_description"] = msg_dict["message"]
         msg_id = msg_dict["id"]
         msg_dict["link"]=f"https://t.me/{chat}/{msg_id}"
+        msg_dict.pop("id")
         data.append(msg_dict)
         #print(msg_dict)
-    return json.dumps(data)
+    return data
+
+async def teleDump():
+  data = await teleQuery(query = None, limit = 100)
+  with open("dump.json",'w') as f:
+    f.write(json.dumps(data, indent=4, sort_keys=True, default=str, ensure_ascii=False))
+
+def dump():
+  import asyncio
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(teleDump())
+
+@api.route('/search', methods=['GET'])
+async def get_results():
+  query = request.args.get("query")
+  data = await teleQuery(query)
+  return json.dumps(data)
 
 @api.route('/')
 def _home():
