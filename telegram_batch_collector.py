ea4c6d8593ec27c0dfcb77d01048413eb3abdb99
Yuri Ostapchuk <twist522@gmail.com>
2022-03-30 23:26:04 +0300

    added lots of channels, batch-writes, cause typesense timeouts often

--- telegram_batch_collector.py
+++ telegram_batch_collector.py
@@ -20,7 +20,7 @@ def telegram_itr_msg_to_post(m: Message) -> Post:
                 category = "other",
                 channel = m.chat.username,
                 channel_id = m.chat.id,
-                channel_title = m.chat.title,
+                channel_title = m.chat.title if hasattr(m.chat, 'title') else m.chat.username,
                 platform = "tg-channel",
                 post_id = m.id,
                 views = m.views,
@@ -30,26 +30,86 @@ def telegram_itr_msg_to_post(m: Message) -> Post:
                 author = m.post_author
                 )
 
+from itertools import islice
+
+# to batch collection into buckets, typesense timeouts on importing big collections
+def batcher(iterable, batch_size):
+    iterator = iter(iterable)
+    while batch := list(islice(iterator, batch_size)):
+        yield batch
 
 if __name__ == '__main__':
 
     # chats = [-1001613584371, -1001601141641]  the other way is to specify chats ids
     chats = [
-        "poshuk_znyklyh",
-        "VolunteerTalksKyiv",
+        "andriysadovyi", 
         "AngelsOfUkraine",
         "ArmyNeeds",
-        "medhelpinfo",
-        "opitpomoshi",
-        "huiiivoiiine",
+        "avtodopomaga",
+        "bazuwork",
+        "boguslav_sos",
+        "burlv",
+        "chornamary", 
+        "DMYTROLUNIN", 
+        "DzvinokDruguBot", 
         "evacuationukraine",
-        "UkrzalInfo",
-        "v_tylu",
-        "pick_up_ukraine",
-        "winwin_realty",
-        "DMYTROLUNIN",
-        "DzvinokDruguBot",
+        "gruz_200_rus", 
+        "help_cv",
+        "helptoukrainegroup",
         "HelpTranskarpatiaLodging",
+        "helpvolonter",
+        "helpzakarpattia",
+        "Hotovyi_do_vsioho_bot",
+        "house_cv",
+        "huiiivoiiine",
+        "ihor_terekhov", 
+        "khersonskaODA", 
+        "khm_gov_ua", 
+        "khmelnytskaODA", 
+        "kiber_oborona_ua",
+        "kozytskyy_maksym_official", 
+        "lobbyx",
+        "lobbyxIT",
+        "luhanskaVTSA", 
+        "Lviv_help",
+        "lviv_stryi",
+        "mariupolrada", 
+        "medhelpinfo",
+        "medlogistic",
+        "nampodorozi_bot",
+        "neruhomistrus", 
+        "opitpomoshi",
+        "palyanycya_zp",
+        "pavlokyrylenko_donoda", 
+        "pick_up_ukraine",
+        "pidvezu_cv", 
+        "poshuk_znyklyh",
+        "realtor_cv", 
+        "rentCV", 
+        "rf200_now", 
+        "saveua_bot",
+        "shostka_volunteer_center",
+        "sm_vca", 
+        "stop_russian_war_bot",
+        "synegubov", 
+        "ternopilskaODA", 
+        "UAhelpinfo",
+        "uajobnow",
+        "ukr_emigration",
+        "UkrzalInfo",
+        "uzhvolunteer",
+        "v_tylu", 
+        "valanteruparus141",
+        "vin_sleep", 
+        "vinnytskaODA", 
+        "VollunteerUA",
+        "volonteri_ua",
+        "volonterniko",
+        "volontery_kh",
+        "volovodii",
+        "VolunteerCountry",
+        "VolunteersHotlineBot",
+        "VolunteerTalks",
         "VolunteerTalksBrovary",
         "VolunteerTalksCherkasy",
         "VolunteerTalksChernihiv",
@@ -58,7 +118,7 @@ if __name__ == '__main__':
         "VolunteerTalksDrohobych",
         "VolunteerTalksGorishniPlavni",
         "VolunteerTalksIF",
-        "VolunteerTalksIrpin",
+        "VolunteerTalksIrpin", 
         "VolunteerTalksKamianske",
         "VolunteerTalksKharkiv",
         "VolunteerTalksKherson",
@@ -74,48 +134,25 @@ if __name__ == '__main__':
         "VolunteerTalksMelitopol",
         "VolunteerTalksMykolaiv",
         "VolunteerTalksOdesa",
-        "VolunteerTalksOleksandriia",
-        "VolunteerTalksOstroh",
+        "VolunteerTalksOleksandriia", 
+        "VolunteerTalksOstroh", 
         "VolunteerTalksPoltava",
         "VolunteerTalksRivne",
+        # "VolunteerTalksRozdilna", # there is some problem with this one
         "VolunteerTalksShid",
         "VolunteerTalksSumy",
         "VolunteerTalksTernopil",
         "VolunteerTalksUzhgorod",
-        "VolunteerTalksVIN",
-        "VolunteerTalksZP",
+        "VolunteerTalksVin",
         "VolunteerTalksZhytomyr",
         "VolunteerTalksZmerynka",
-        "Zhyvytskyy",
-        "andriysadovyi",
-        "chornamary",
-        "gruz_200_rus",
-        "helpzakarpattia",
-        "house_cv",
-        # "housing_cv",
-        "ihor_terekhov",
-        "khersonskaODA",
-        "khm_gov_ua",
-        "khmelnytskaODA",
-        "kozytskyy_maksym_official",
-        "luhanskaVTSA",
-        "mariupolrada",
-        "neruhomistrus",
-        "pavlokyrylenko_donoda",
-        "pidvezu_cv",
-        "realtor_cv",
-        "rentCV",
-        "rf200_now",
-        "sm_vca",
-        "synegubov",
-        "ternopilskaODA",
-        "vin_sleep",
-        "vinnytskaODA",
-        "volynskaODA",
-        "zak_help",
-        "lobbyx",
-        "kiber_oborona_ua",
-        "lobbyxIT"
+        "VolunteerTalksZP",
+        "volynskaODA", 
+        "vszu_ua",
+        "winwin_realty",
+        "zak_help", 
+        # "zevolunteers", # same story
+        "Zhyvytskyy", 
     ]
 
     load_dotenv()
@@ -155,13 +192,16 @@ if __name__ == '__main__':
                                                   limit=int(os.getenv("TYPESENSE_BATCH_LOAD_MESSAGE_LIMIT")),
                                                   min_id=last_id
                                                   )
-                messages = list(map(telegram_itr_msg_to_post, iter_msgs))
-                if len(messages) > 0:
-                    non_empty_msgs = list(filter(lambda x: x.is_non_empty_message(), messages))
-                    message_dicts = list(map(lambda x: x.__dict__, non_empty_msgs))
-                    import_result = typesense_client.collections["posts"].documents.import_(message_dicts, params={"batch_size": 100})
-                    print(
-                        f"[TYPESENSE][IMPORT] from tg channel: {chat}, {len(non_empty_msgs)} messages,"
-                        + f"result = {import_result[:10]}....")
+                all_messages = list(filter(lambda post: (len(post.message) if post.message is not None else 0) >= 50,
+                                           map(telegram_itr_msg_to_post, iter_msgs)))
+                if len(all_messages) > 0:
+                    for messages in batcher(all_messages, 100):
+                        non_empty_msgs = list(filter(lambda x: x.is_non_empty_message(), messages))
+                        message_dicts = list(map(lambda x: x.__dict__, non_empty_msgs))
+                        import_result = typesense_client.collections["posts"].documents.import_(message_dicts, params={"batch_size": 50})
+                        print(
+                            f"[TYPESENSE][IMPORT] from tg channel: {chat}, {len(non_empty_msgs)} messages,"
+                            + f"result = {import_result[:10]}....")
                 else:
                     print("nothing to scrape, all set")
+
